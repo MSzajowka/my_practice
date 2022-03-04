3ece48fe1b4b189ff17463bf755b366494080df7
MSzajowka <m.szajowka@gmail.com>
2022-03-04 10:50:27 +0100

    excercise 1

---
+++ practice1.py
@@ -0,0 +1,15 @@
+def list_keys(d):
+    keys_list = []
+    for key in d:
+        print(key)
+
+
+d = {
+    'one': 'jeden',
+    'two': 'dwa',
+    'three': 'trzy',
+    'four': 'cztery',
+    'five': 'piec'
+}
+
+list_keys(d)
