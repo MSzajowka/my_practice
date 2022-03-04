7aa83c7697dbe33c745b6fe4740bd298313662c9
MSzajowka <m.szajowka@gmail.com>
2022-03-04 13:48:00 +0100

    example1

--- example1.py
+++ example1.py
@@ -4,3 +4,10 @@ hobbit = {
 }
 description = "%(name)s is a %(occupation)s." % hobbit
 print(description)
+
+description = "{} is a {}.".format("Gandalf The Grey", "wizard")
+print(description)
+
+dead = 'Sauron'
+description = f'{dead} is defeated.'
+print(description)
