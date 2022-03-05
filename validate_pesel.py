6b7517a4b2817241cd795fcf9456713977b9fb1a
MSzajowka <m.szajowka@gmail.com>
2022-03-05 17:53:27 +0100

    practice 13 - function that validates if PESEL is correct

---
+++ validate_pesel.py
@@ -0,0 +1,13 @@
+def validate_pesel(pesel):
+    weight = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+    pesel = [int(a) for a in str(pesel)]
+    control = []
+    for i in range(10):
+        control.append(weight[i] * pesel[i])
+    control = sum(control)
+    if 10 - (control % 10) == pesel[-1]:
+        return True
+    return False
+
+
+print(validate_pesel((84102900875)))
